3a7f4f3121f7359c20fc2eb527ce97ca619900f3
Jannaee Sick <jannaee@gmail.com>
2020-10-18 00:33:47 +0000

    Adding Carbon

--- crushed-bugs/src/App.js
+++ crushed-bugs/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ATOForm from './components/ATOForm';
+import { Grid, Row, Column } from 'carbon-components-react';
+
 import './app.scss';
 
 function App() {
   return (
-    <div>
+    <>
+
       <ATOForm />
-    </div>
+
+    </>
   );
 }
 

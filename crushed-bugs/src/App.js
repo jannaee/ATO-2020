38a7699de693082a3ae675def2c6c8a4654344da
Jannaee Sick <jannaee@gmail.com>
2020-10-18 00:19:44 +0000

    Setting up simple form

--- crushed-bugs/src/App.js
+++ crushed-bugs/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Button} from 'carbon-components-react';
+import ATOForm from './components/ATOForm';
 import './app.scss';
 
 function App() {
   return (
     <div>
-      <Button>Hello</Button>
+      <ATOForm />
     </div>
   );
 }
